03500f4269d33c43c671a193ae3106045c84ae62
seanbaang <dev.seanbaang@gmail.com>
2022-07-25 18:28:32 +0800

    Added Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isEmpty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'training-angular';
+  title = 'My First Angular App';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { id: Math.random(), description: 'eat', done: true },
+    { id: Math.random(), description: 'sleep', done: false },
+    { id: Math.random(), description: 'play', done: false },
+    { id: Math.random(), description: 'laugh', done: false },
+  ]
+
+  get items() {
+
+    if(this.filter === 'all'){
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string){
+    if(description == ''){
+      
+    } else {
+      this.allItems.unshift({
+        id: Math.random(),
+        description,
+        done: false
+      });
+    }
+  }
+
+  remove(item: { id: number, description: string; done: boolean; }) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
